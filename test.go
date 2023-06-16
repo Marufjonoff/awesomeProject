f8e1f0d29b3c2ea7d456226da8231c6664cf2202
Marufjonoff <marufjonovobidjon91@gmail.com>
2023-06-16 10:09:28 +0500

    test

--- test.go
+++ test.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	print(Hello("Obidjondan salom"))
+}
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
