43b0041f0cbac8db29c8ce0775b95331a1727486
Marufjonoff <marufjonovobidjon91@gmail.com>
2023-06-16 10:22:30 +0500

    decimal to binary

--- test.go
+++ test.go
@@ -4,10 +4,29 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
-	print(Hello("Obidjondan salom"))
+	fmt.Println(Hello("Obidjondan salom"))
+	decimalToBinary(123)
 }
 
 func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
+}
+
+func decimalToBinary(num int) {
+	var binary []int
+
+	for num != 0 {
+		binary = append(binary, num%2)
+		num = num / 2
+	}
+
+	if len(binary) == 0 {
+		fmt.Printf("%d\n", 0)
+	} else {
+		for i := len(binary) - 1; i >= 0; i-- {
+			fmt.Printf("%d", binary[i])
+		}
+		fmt.Println()
+	}
 }
